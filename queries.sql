c9554190e53af882da6dc6ac42386a73d4cdaabb
cs5004spr20 <60734111+cs5004spr20@users.noreply.github.com>
2020-10-10 16:23:11 -0600

    Updated to include all queries

--- queries.sql
+++ queries.sql
@@ -34,3 +34,37 @@ WHERE
     AND c.closureDate IS NOT NULL
 GROUP BY i.`name`
 HAVING `messages sent` < 14;
+
+-- Get the id and number of cases assigned to each case manager.
+SELECT cm.person_id AS CM_ID, CASE WHEN CaseCount IS NULL THEN 0 ELSE CaseCount END AS CaseCount
+FROM casemanagers cm 
+LEFT JOIN (SELECT cm_person_id,COUNT(cm_person_id) AS CaseCount FROM cases GROUP BY cm_person_id) counts 
+ON cm_person_id = cm.person_id;
+
+-- Get the id, name, phone, and email of individuals who have been
+-- in contact with a tracked individual but do not have a case yet.
+SELECT person_id, ind.name, ind.phone, ind.email
+FROM individuals ind 
+JOIN (SELECT DISTINCT(contact_person_id) AS contact, cases.case_id 
+	FROM contactlogs 
+	LEFT JOIN cases ON contact_person_id = tracked_person_id WHERE cases.case_id IS NULL) logged 
+ON person_id = contact;
+
+-- Get the case, name, preferred communication method, contact information, 
+-- and case closure date for tracked individuals who have a temperature over
+-- 99 degrees F and are experiencing a cough or breathing problems.
+SELECT cases.case_id, ind.name, commMethod, 
+	CASE WHEN commMethod='Email' THEN email ELSE phone END AS contactInfo, closureDate 
+FROM cases 
+JOIN (SELECT DISTINCT(case_id) FROM monitoringlogs ml 
+	JOIN symptomlist sl ON sl.monitor_id = ml.monitor_id
+	WHERE temp > 99 AND tempType = 'F' AND symptom_id IN (3,4,5)) log
+ON cases.case_id = log.case_id 
+JOIN communicationmethods cm ON cases.commMethod_id = cm.commMethod_id
+JOIN individuals ind ON tracked_person_id = person_id;
+
+-- Find the id of individuals that were not infected and have been re-exposed.
+-- Include the date the original case was closed, and the new exposure date.
+SELECT tracked_person_id, closureDate AS originalClosureDate, timestamp AS exposureDate
+FROM cases JOIN contactlogs ON tracked_person_id = contact_person_id 
+WHERE infected = 0 AND timestamp > closureDate;
